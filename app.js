cff6b1591f27bb4c5e606e9d149a9fa5dadd02ad
Maggie Gutierrez <maggie.vibes@icloud.com>
2026-06-15 20:31:00 -0600

    New folders 'controllers' and 'models'. Starting the project.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3000 } = process.env;
 const userRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/mydb");
+    console.log(`Express ahora sí se ha conectado`);
+  } catch (error) {
+    console.log(`Error al conectar a MongoDB: ${error.message}`);
+  }
+};
 
 app.use("/users", userRouter);
 app.use("/cards", cardsRouter);
--- routes/cards.js
+++ routes/cards.js
@@ -12,6 +12,21 @@ router.get("/", (req, res) => {
         res.status(500).json({ error: "Fallo en cargar las tarjetas" });
       } else {
         const cards = JSON.parse(data);
+        cards
+          .find({ title: "nonexistant card" })
+          .orFail(() => {
+            const error = new Error(
+              "No se ha encontrado ninguna tarjeta con esa id",
+            );
+            error.statusCode = 404;
+            throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+          }) // arroja un DocumentNotFoundError
+          .then((cardData) => {
+            res.send(cardData); // omitido porque se arrojó un error
+          })
+          .catch((error) => {
+            // este sí se ejecuta ahora, así que podemos manejar el error y devolver un mensaje adecuado
+          });
         res.json(cards);
       }
     },
